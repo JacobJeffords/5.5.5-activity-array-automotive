6cb9a9bf62fb30d00f14b5fb3f52d64bc1e33e5c
Jacob Jeffords <jacobjeffords64@gmail.com>
2024-03-25 15:12:42 -0400

    Completed assignment

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,5 +1,5 @@
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicleBaseClass")
+const VehicleModule = require("./vehicleBaseClass").Vehicle
 
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mercury", "Sedan", "1965", "color", "mileage");
@@ -12,6 +12,46 @@ console.log(v.make)
 
 
 //TO DO: Code the Car subclass here or in index.js file, i.e. class Car extends Vehicle ...
+
+class Car extends VehicleModule {
+    constructor(make, model, year, color, mileage) {
+        super(make, model, year, color, mileage);
+        this.fuel = 10;
+        this.maxPassengers = 5;
+        this.numberOfWheels = 4;
+        this.passenger = 0;
+        this.maxSpeed = 160;
+        this.scheduleService = false;
+        console.log("test")
+    }
+    loadPassenger() {
+        if (this.passenger < this.maxPassengers) {
+            this.passenger = loadPassenger
+            console.log("Passengers loaded. Current passenger count: ") + loadPassenger
+        } else {
+            console.log("Cannot load more passengers, car is full. Current passenger count: ") + loadPassenger
+        }
+    }
+    start() {
+        if (this.fuel > 0) {
+            console.log("Vehicle sufficiently fueled. Engine has started.")
+            return this.started = true
+        } else {
+            console.log("Not enough fuel. Engine has not started.")
+            return this.started = false
+        }
+    }
+    service() {
+        if (this.mileage > 30000) {
+            console.log("Service automatically scheduled.")
+            this.scheduleService = true
+            return this.scheduleService;
+        }
+    }
+}
+
+let myCar = new Car("Mercury", "Sedan", "1965", "color", "mileage")
+console.log(myCar)
 
 
 
